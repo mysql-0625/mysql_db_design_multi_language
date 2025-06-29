1ca1497b1c17043f3ffd81a6fcc49be487214c92
mhasan8801 <hasanmuhammad197@gmail.com>
2025-06-29 09:44:30 +0700

    add config multi language

--- mysql_multi_language.sql
+++ mysql_multi_language.sql
@@ -37,6 +37,40 @@ DESC categories_translation;
 ALTER TABLE categories_translation
 	ADD CONSTRAINT fk_categories_translation
 		FOREIGN KEY (category_id) REFERENCES categories(id);
+        
+-- Insert data translasi        
+INSERT INTO categories_translation (category_id, language, name, description)
+VALUES ("ELECTRONICS", "in_ID", "Elektronik", "Ini deskripsi elektronik"),
+		("ELECTRONICS", "en_US", "Electronics", "This is an electronics description"),
+		("ELECTRONICS", "zh_CN", "电子产品", "这是一个关于电子产品的描述"),
+        ("FOOD", "in_ID", "Makanan", "Ini deskripsi makanan"),
+		("FOOD", "en_US", "Food", "This is a food description"),
+		("FOOD", "zh_CN", "食品 ", "这是一个关于食品的描述"),
+        ("GADGET", "in_ID", "Gawai", "Ini deskripsi gawai"),
+		("GADGET", "en_US", "Gadget", "This is a gadget description"),
+		("GADGET", "zh_CN", "小工具", "这是一个关于小工具的描述");
+        
+-- Select semua category semua translasi        
+SELECT * FROM categories
+JOIN categories_translation ct on categories.id = ct.category_id;   
+
+-- Select semua category berdasarkan bahasa indonesia       
+SELECT * FROM categories
+JOIN categories_translation ct on categories.id = ct.category_id
+WHERE ct.language = "in_ID" ORDER BY position;  
+
+-- Select semua category berdasarkan bahasa english       
+SELECT * FROM categories
+JOIN categories_translation ct on categories.id = ct.category_id
+WHERE ct.language = "en_US" ORDER BY position;   
+
+-- Select semua category berdasarkan bahasa china       
+SELECT * FROM categories
+JOIN categories_translation ct on categories.id = ct.category_id
+WHERE ct.language = "zh_CN" ORDER BY position; 
+
+     
+        
 
 
 
